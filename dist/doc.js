3485cfb7d25b966b60502dddf06a1cb85faa3fa0
moeenn <moeen.v8@gmail.com>
2021-02-10 00:11:29 +0500

    performed testing

--- dist/doc.js
+++ dist/doc.js
@@ -1,148 +1,194 @@
-window doc = class Doc {
+"use strict";
+
+window.doc = class Doc {
 	/**
 	 *  shortcut for document.querySelector
 	 *  with error handling
 	 *
-	*/
-	static function select(selector) {
-	  const elem = document.querySelector(selector);
-	  if (!elem) throw `Element with selector "${selector}" not found.`;
-	  return elem;
+	 */
+	static select(selector) {
+		const elem = document.querySelector(selector);
+		if (!elem) throw `Element with selector "${selector}" not found.`;
+		return elem;
 	}
-
 
 	/**
 	 *  shortcut for document.querySelectorAll
 	 *  with error handling
 	 *
-	*/
-	static function selectAll(selector) {
-	  const elements = document.querySelectorAll(selector);
-	  if (!elements) throw `Elements with selector "${selector}" not found.`;
-	  return elements;
+	 */
+	static selectAll(selector) {
+		const elements = document.querySelectorAll(selector);
+		if (!elements || elements.length === 0)
+			throw `Elements with selector "${selector}" not found.`;
+		return elements;
 	}
-
 
 	/**
 	 *  check if element exists
 	 *
-	*/
-	static function exists(selector) {
-	  return document.querySelector(selector) === null;
+	 */
+	static exists(selector) {
+		return document.querySelector(selector) !== null;
 	}
-
 
 	/**
 	 *  add event listener to element with it's selector
 	 *
-	*/
-	static function listen(selector, event_name, callback) {
-	  const element = this.constructor.select(selector);
+	 */
+	static listen(selector, event_name, callback) {
+		const element = this.select(selector);
 
-	  if (element) {
-	    element.addEventListener(event_name, callback);
-	  }
+		if (element) {
+			element.addEventListener(event_name, callback);
+		}
 	}
-
 
 	/**
 	 *  find element height
 	 *
-	*/
-	static function height(element) {
-	  return element.offsetHeight;
+	 */
+	static height(element) {
+		return element.offsetHeight;
 	}
-
 
 	/**
 	 *  find element width
 	 *
-	*/
-	static function width(element) {
-	  return element.offsetWidth;
+	 */
+	static width(element) {
+		return element.offsetWidth;
 	}
-
 
 	/**
 	 *  get element position relative to top of the page
 	 *
-	*/
-	static function distanceFromTop(element) {
-	  const rect = element.getBoundingClientRect();
-	  const scrollTop = static function pageYOffset || document.documentElement.scrollTop;
-	  return (rect.top + scrollTop);
+	 */
+	static distanceFromTop(element) {
+		const rect = element.getBoundingClientRect();
+		const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return rect.top + scrollTop;
 	}
-
 
 	/**
 	 *  smooth scroll to element
 	 *
-	*/
-	static function scrollToElement(element, options = {}) {
-	  const default_options = {
-	    smooth: true,
-	    topOffset: 0,
-	  };
+	 */
+	static scrollToElement(element, options = {}) {
+		const default_options = {
+			smooth: true,
+			topOffset: 0,
+		};
 
-	  Object.assign(default_options, options);
+		Object.assign(default_options, options);
 
-	  const topPixels = this.constructor.distanceFromTop(element);
+		const topPixels = this.distanceFromTop(element);
 
-	  static function scroll({
-	    top: topPixels - default_options.topOffset,
-	    behavior: (default_options.smooth) ? 'smooth' : 'auto',
-	  })
+		window.scroll({
+			top: topPixels - default_options.topOffset,
+			behavior: default_options.smooth ? "smooth" : "auto",
+		});
 	}
 
+	/**
+	 *  scroll to the top of the document
+	 *
+	 */
+	static scrollToTop(options = {}) {
+		const default_options = {
+			smooth: true,
+			topOffset: 0,
+		};
+
+		Object.assign(default_options, options);
+		window.scroll({
+			top: default_options.topOffset,
+			behavior: default_options.smooth ? "smooth" : "auto",
+		});
+	}
 
 	/**
 	 *  get all data attributes for an element
 	 *
-	*/
-	static function getData(element, data_name = null) {
-	  if (data_name) return element.dataset[data_name];
-	  return element.dataset;
+	 */
+	static getData(element, data_name = null) {
+		if (data_name) return element.dataset[data_name];
+		return element.dataset;
 	}
-
 
 	/**
 	 *  get all attributes for an element
 	 *
-	*/
-	static function getAttibutes(element, attriute_name = null) {
-	  const attributes = element.attributes;
+	 */
+	static getAttibutes(element, attriute_name = null) {
+		const attributes = element.attributes;
 
-	  if (attriute_name) {
-	    return attributes[attriute_name].textContent;
-	  }
+		if (attriute_name) {
+			return attributes[attriute_name].textContent;
+		}
 
-	  const result = [];
+		const result = [];
 
-	  for (let i = 0; i < attributes.length; i++) {
-	    result.push({
-	      attribute: attributes[i].name,
-	      value: attributes[i].textContent
-	    });
-	  }
+		for (let i = 0; i < attributes.length; i++) {
+			result.push({
+				attribute: attributes[i].name,
+				value: attributes[i].textContent,
+			});
+		}
 
-	  return result;
+		return result;
 	}
 
+	/**
+	 *  apply styles to element
+	 *
+	 */
+	static applyStyles(element, styles) {
+		Object.assign(element.style, styles);
+	}
+
+	/**
+	 *  add css classes to document
+	 *
+	 */
+	static applyStyles(element, styles) {
+		Object.assign(element.style, styles);
+	}
 
 	/**
 	 *  hide elements from DOM
 	 *
-	*/
-	static function hide(element) {
-	  element.style.display = 'none';
-	}
+	 */
+	static hide(el) {
+		el.style.opacity = 1;
 
+		(function fade() {
+			if ((el.style.opacity -= 0.1) < 0) {
+				el.style.display = "none";
+			} else {
+				requestAnimationFrame(fade);
+			}
+		})();
+
+		el.hidden = true;
+	}
 
 	/**
 	 *  show hidden elements in DOM
 	 *
-	*/
-	static function show(element) {
-	  element.style.display = 'contents';
+	 */
+	static show(el, display) {
+		el.style.opacity = 0;
+		el.style.display = display || "block";
+
+		(function fade() {
+			var val = parseFloat(el.style.opacity);
+			if (!((val += 0.1) > 1)) {
+				el.style.opacity = val;
+				requestAnimationFrame(fade);
+			}
+		})();
+
+		el.hidden = false;
 	}
 };
